fde8401bc58fcc797768bc1fbb7df0306277dc3a
Andras Mihaly <andrasm96@gmail.com>
2019-10-22 12:40:34 -0700

    Added text for the adventure

--- main.go
+++ main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Space Adventure, Baby.\n")
+	fmt.Printf("Welcome to the Solar System!\n")
+	fmt.Printf("There are nine planets to explore.\n")
+	fmt.Printf("What is your name?\n")
+	fmt.Printf("Nice to meet you [insert name]! My name is Eliza, I am an old friend of Alexa.\n")
+	fmt.Printf("Lets go on an adventure!\n")
+	fmt.Printf("Shall I randomly choose a planet for you to visit? (Y or N)\n")
+	fmt.Printf("I'm sorry I didn't get that.\n")
+	fmt.Printf("Please enter the name of the planet you would like to visit.\n")
+	fmt.Printf("Welcome to the Solar System!\n")
+	fmt.Printf("Travelling to [insert planet]...\n")
+	fmt.Printf("Welcome to Mars\n")
+	fmt.Printf("Welcome to Saturn\n")
+	fmt.Printf("Welcome to Neptune\n")
+	fmt.Printf("Welcome to Uranus\n")
+	fmt.Printf("Welcome to Pluto\n")
+	fmt.Printf("Welcome to Venus\n")
+	fmt.Printf("Welcome to Jupiter\n")
+	fmt.Printf("Welcome to Mercury\n")
+	fmt.Printf("Welcome to Earth\n")
+	fmt.Printf("Welcome to Null PlAnEt\n")
+
 }
